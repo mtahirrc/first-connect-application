4016a1ddb0c0f5fb55901e9b5281e2b14bad5e4d
mtahirrc <146178769+mtahirrc@users.noreply.github.com>
2024-02-21 08:54:50 +0500

    service added

--- service/src/routes/service.route.js
+++ service/src/routes/service.route.js
@@ -4,7 +4,7 @@ import { logger } from '../utils/logger.utils.js';
 const serviceRouter = Router();
 
 serviceRouter.post('/', async (req, res) => {
-  logger.info('Cart update extension executed',req);
+  logger.info('Cart update extension executed');
   res.status(200);
   res.send();
 });
